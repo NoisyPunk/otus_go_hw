6de94adf50ed6e14bac84f7e2cb01910f3fde306
isolodovnikov <mnoisypunk@gmail.com>
2023-12-12 15:43:32 +0300

    HW2 linter fixes

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -19,21 +19,16 @@ func Unpack(s string) (string, error) {
 			return "", ErrInvalidString
 		}
 		if unicode.IsLetter(value) {
-			result = result + string(value)
+			result += string(value)
 			continue
 		}
 		if unicode.IsDigit(value) && !unicode.IsDigit(runeSlice[i-1]) && int(value-'0') > 1 {
-			t1 := string(runeSlice[i-1])
-			t2 := value - '0'
-			_, _ = t1, t2
-			result = result + strings.Repeat(string(runeSlice[i-1]), int((value-'0')-1))
+			result += strings.Repeat(string(runeSlice[i-1]), int((value-'0')-1))
 		}
 		if unicode.IsDigit(value) && !unicode.IsDigit(runeSlice[i-1]) && int(value-'0') <= 1 {
-			result = result + string(value)
+			result += string(value)
 			result = strings.ReplaceAll(result, string(runeSlice[i-1:i+1]), "")
 		}
-
 	}
-
 	return result, nil
 }
