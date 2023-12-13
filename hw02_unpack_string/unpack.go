5263810252a12b73148b644fce483080a372badd
isolodovnikov <mnoisypunk@gmail.com>
2023-12-13 19:07:39 +0300

    HW2 fixes for review

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,21 +11,20 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(s string) (string, error) {
 	runeSlice := []rune(s)
 	var result string
+
+	if s != "" && unicode.IsDigit(runeSlice[0]) {
+		return "", ErrInvalidString
+	}
+
 	for i, value := range runeSlice {
-		if unicode.IsDigit(value) && i == 0 {
-			return "", ErrInvalidString
-		}
-		if unicode.IsDigit(value) && unicode.IsDigit(runeSlice[i-1]) {
-			return "", ErrInvalidString
-		}
-		if unicode.IsLetter(value) {
+		switch {
+		case unicode.IsLetter(value):
 			result += string(value)
-			continue
-		}
-		if unicode.IsDigit(value) && !unicode.IsDigit(runeSlice[i-1]) && int(value-'0') > 1 {
+		case unicode.IsDigit(value) && unicode.IsDigit(runeSlice[i-1]):
+			return "", ErrInvalidString
+		case unicode.IsDigit(value) && !unicode.IsDigit(runeSlice[i-1]) && int(value-'0') > 1:
 			result += strings.Repeat(string(runeSlice[i-1]), int((value-'0')-1))
-		}
-		if unicode.IsDigit(value) && !unicode.IsDigit(runeSlice[i-1]) && int(value-'0') <= 1 {
+		case unicode.IsDigit(value) && !unicode.IsDigit(runeSlice[i-1]) && int(value-'0') <= 1:
 			result += string(value)
 			result = strings.ReplaceAll(result, string(runeSlice[i-1:i+1]), "")
 		}
