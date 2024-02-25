7636b2317d0edfd6deebc1e0cb0f39218756a428
isolodovnikov <mnoisypunk@gmail.com>
2024-02-25 22:54:46 +0300

    HW 10: optimization take 2

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -9,6 +9,11 @@ import (
 	"github.com/mailru/easyjson"
 )
 
+var (
+	ErrEmptyDomain = fmt.Errorf("empty domain")
+	ErrEmptyData   = fmt.Errorf("empty data")
+)
+
 //easyjson:json
 type User struct {
 	ID       int
@@ -23,36 +28,42 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
+	u, err := getEmails(r)
 	if err != nil {
 		return nil, fmt.Errorf("get users error: %w", err)
 	}
 	return countDomains(u, domain)
 }
 
-type users []User
+type emails []string
 
-func getUsers(r io.Reader) (result users, err error) {
+func getEmails(r io.Reader) (result emails, err error) {
+	if r == nil {
+		return nil, ErrEmptyData
+	}
 	s := bufio.NewScanner(r)
+	user := User{}
 
 	for s.Scan() {
-		var user User
 		if err = easyjson.Unmarshal(s.Bytes(), &user); err != nil {
 			return
 		}
-		result = append(result, user)
+		result = append(result, user.Email)
 	}
 	return
 }
 
-func countDomains(u users, domain string) (DomainStat, error) {
+func countDomains(u emails, domain string) (DomainStat, error) {
 	result := make(DomainStat)
+	if domain == "" {
+		return nil, ErrEmptyDomain
+	}
 
-	for _, user := range u {
-		if user.Email == "" {
+	for _, email := range u {
+		if email == "" {
 			continue
 		}
-		mailDomain := strings.SplitAfter(user.Email, "@")
+		mailDomain := strings.SplitAfter(email, "@")
 		if strings.HasSuffix(mailDomain[1], domain) {
 			result[strings.ToLower(mailDomain[1])]++
 		}
