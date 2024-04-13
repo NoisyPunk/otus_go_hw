75d63577dd1dc3edc6e02bd715ef5e7d442aed15
isolodovnikov <mnoisypunk@gmail.com>
2024-04-13 18:57:07 +0300

    HW 15: Update integration tests

--- hw12_13_14_15_calendar/internal/storage/sql/storage.go
+++ hw12_13_14_15_calendar/internal/storage/sql/storage.go
@@ -20,7 +20,7 @@ var (
 )
 
 type Storage struct {
-	db *sqlx.DB
+	DB *sqlx.DB
 }
 
 func New() *Storage {
@@ -28,15 +28,15 @@ func New() *Storage {
 }
 
 func (s *Storage) Connect(ctx context.Context, dsn string) (err error) {
-	s.db, err = sqlx.Open("postgres", dsn)
+	s.DB, err = sqlx.Open("postgres", dsn)
 	if err != nil {
 		return errors.Wrap(err, ErrDBConnection.Error())
 	}
-	return s.db.PingContext(ctx)
+	return s.DB.PingContext(ctx)
 }
 
 func (s *Storage) Close() error {
-	return s.db.Close()
+	return s.DB.Close()
 }
 
 func (s *Storage) Create(ctx context.Context, data storage.Event, userID uuid.UUID) (storage.Event, error) {
@@ -56,7 +56,7 @@ func (s *Storage) Create(ctx context.Context, data storage.Event, userID uuid.UU
 		TimeToNotify: data.TimeToNotify,
 	}
 
-	_, err := s.db.NamedQuery(query, event)
+	_, err := s.DB.NamedQuery(query, event)
 	if err != nil {
 		l.Error(err.Error(), zap.String("event_id:", eventID.String()))
 		return storage.Event{}, errors.Wrap(err, ErrCreateEvent.Error())
@@ -71,7 +71,7 @@ func (s *Storage) Update(ctx context.Context, eventID uuid.UUID, event storage.E
 	query := `UPDATE events SET (user_id, title, date_and_time, duration, description, time_to_notify) =
 				(:user_id, :title, :date_and_time, :duration, :description, :time_to_notify) WHERE id=:id`
 
-	_, err := s.db.NamedQuery(query, &event)
+	_, err := s.DB.NamedQuery(query, &event)
 	if err != nil {
 		l.Error(err.Error(), zap.String("event_id:", eventID.String()))
 		return err
@@ -85,7 +85,7 @@ func (s *Storage) Delete(ctx context.Context, eventID uuid.UUID) error {
 
 	query := `DELETE FROM events WHERE id=$1`
 
-	_, err := s.db.Exec(query, eventID)
+	_, err := s.DB.Exec(query, eventID)
 	if err != nil {
 		l.Error(err.Error(), zap.String("event_id:", eventID.String()))
 		return err
@@ -100,7 +100,7 @@ func (s *Storage) DailyList(ctx context.Context, date time.Time, userID uuid.UUI
 	var events []storage.Event
 	query := `SELECT * FROM events where user_id = $1 and date_and_time between $2 and $2 + INTERVAL '1 day'`
 
-	err := s.db.Select(&events, query, userID, date)
+	err := s.DB.Select(&events, query, userID, date)
 	if err != nil {
 		return nil, err
 	}
@@ -114,7 +114,7 @@ func (s *Storage) WeeklyList(ctx context.Context, startWeekDate time.Time, userI
 	var events []storage.Event
 	query := `SELECT * FROM events where user_id = $1 and date_and_time between $2 and $2 + INTERVAL '1 week'`
 
-	err := s.db.Select(&events, query, userID, startWeekDate)
+	err := s.DB.Select(&events, query, userID, startWeekDate)
 	if err != nil {
 		return nil, err
 	}
@@ -129,7 +129,7 @@ func (s *Storage) MonthlyList(ctx context.Context, startMonthDate time.Time,
 	var events []storage.Event
 	query := `SELECT * FROM events where user_id = $1 and date_and_time between $2 and $2 + INTERVAL '1 month'`
 
-	err := s.db.Select(&events, query, userID, startMonthDate)
+	err := s.DB.Select(&events, query, userID, startMonthDate)
 	if err != nil {
 		return nil, err
 	}
@@ -145,7 +145,7 @@ func (s *Storage) DeleteOldEvents(ctx context.Context, storagePeriod int) error 
 
 	query := `SELECT id FROM events where date_and_time < $1`
 
-	err := s.db.Select(&events, query, period)
+	err := s.DB.Select(&events, query, period)
 	if err != nil {
 		return err
 	}
@@ -166,7 +166,7 @@ func (s *Storage) NotifyList(ctx context.Context) ([]storage.Event, error) {
 			FROM events where  date_and_time > now() + make_interval(hours := 3) 
               and now() + make_interval(hours := 3) + make_interval(mins := time_to_notify) > date_and_time`
 
-	err := s.db.Select(&events, query)
+	err := s.DB.Select(&events, query)
 	if err != nil {
 		return nil, err
 	}
--- hw12_13_14_15_calendar/tests/integration_test.go
+++ hw12_13_14_15_calendar/tests/integration_test.go
@@ -2,10 +2,16 @@ package internalhttp
 
 import (
 	"bytes"
+	"context"
+	"github.com/NoisyPunk/otus_go_hw/hw12_13_14_15_calendar/internal/logger"
+	"github.com/NoisyPunk/otus_go_hw/hw12_13_14_15_calendar/internal/storage"
+	sqlstorage "github.com/NoisyPunk/otus_go_hw/hw12_13_14_15_calendar/internal/storage/sql"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	"io"
 	"net/http"
 	"testing"
+	"time"
 )
 
 func TestCreateEventHandler(t *testing.T) {
@@ -90,4 +96,211 @@ func TestCreateUpdateHandler(t *testing.T) {
 			require.Equal(t, c.responseCode, r.StatusCode)
 		})
 	}
+}
+
+func TestEvent(t *testing.T) {
+	log := logger.New("debug")
+	ctx := logger.ContextLogger(context.Background(), log)
+
+	DBstorage := sqlstorage.New()
+
+	dsn := "host=testdb.local port=5432 user=postgres password=postgres dbname=calendar sslmode=disable"
+
+	err := DBstorage.Connect(ctx, dsn)
+	require.NoError(t, err)
+
+	t.Run("create event", func(t *testing.T) {
+		userID := uuid.New()
+
+		expectedEvent := storage.Event{
+			Title:        "Test Title",
+			DateAndTime:  time.Date(2024, 11, 5, 23, 24, 23, 0, time.UTC),
+			Duration:     15,
+			Description:  "Test Description",
+			UserID:       userID,
+			TimeToNotify: 10,
+		}
+
+		id, err := DBstorage.Create(ctx, expectedEvent, userID)
+		require.NotNil(t, id)
+
+		expectedEvent.ID = id.ID
+		require.NoError(t, err)
+
+		var events []*storage.Event
+		query := `SELECT * FROM events where user_id = $1`
+		err = DBstorage.DB.Select(&events, query, expectedEvent.UserID)
+		require.NoError(t, err)
+		require.Equal(t, &expectedEvent, events[0])
+	})
+	t.Run("update event", func(t *testing.T) {
+		userID := uuid.New()
+
+		expectedEvent := storage.Event{
+			Title:        "Test Title",
+			DateAndTime:  time.Date(2024, 11, 5, 23, 24, 23, 0, time.UTC),
+			Duration:     15,
+			Description:  "Test Description",
+			UserID:       userID,
+			TimeToNotify: 10,
+		}
+
+		id, err := DBstorage.Create(ctx, expectedEvent, userID)
+		require.NoError(t, err)
+		require.NotNil(t, id)
+
+		event := storage.Event{
+			ID:           id.ID,
+			Title:        "Test Title Updated",
+			DateAndTime:  time.Date(2024, 11, 5, 23, 24, 23, 0, time.UTC),
+			Duration:     15,
+			Description:  "Test Description Updated",
+			UserID:       userID,
+			TimeToNotify: 10,
+		}
+
+		err = DBstorage.Update(ctx, id.ID, event)
+		require.NoError(t, err)
+
+		var events []*storage.Event
+		query := `SELECT * FROM events where id = $1`
+		err = DBstorage.DB.Select(&events, query, id.ID)
+		require.NoError(t, err)
+		require.Equal(t, &event, events[0])
+	})
+	t.Run("delete event", func(t *testing.T) {
+		userID := uuid.New()
+
+		expectedEvent := storage.Event{
+			Title:        "Test Title",
+			DateAndTime:  time.Date(2024, 11, 5, 23, 24, 23, 0, time.UTC),
+			Duration:     15,
+			Description:  "Test Description",
+			UserID:       userID,
+			TimeToNotify: 10,
+		}
+
+		id, err := DBstorage.Create(ctx, expectedEvent, userID)
+		require.NoError(t, err)
+		require.NotNil(t, id)
+
+		err = DBstorage.Delete(ctx, id.ID)
+		require.NoError(t, err)
+
+		query := `SELECT * FROM events where id = $1`
+		row, err := DBstorage.DB.Exec(query, expectedEvent.UserID)
+		require.NoError(t, err)
+		c, err := row.RowsAffected()
+		require.NoError(t, err)
+		require.Zero(t, c)
+	})
+}
+
+func TestEventLists(t *testing.T) {
+	log := logger.New("debug")
+	ctx := logger.ContextLogger(context.Background(), log)
+
+	DBstorage := sqlstorage.New()
+
+	dsn := "host=testdb.local port=5432 user=postgres password=postgres dbname=calendar sslmode=disable"
+
+	err := DBstorage.Connect(ctx, dsn)
+	require.NoError(t, err)
+
+	t.Run("daily list", func(t *testing.T) {
+		userID := uuid.New()
+
+		events := []storage.Event{
+			{
+				Title:        "Test Title",
+				DateAndTime:  time.Date(2024, 11, 6, 23, 25, 23, 0, time.UTC),
+				Duration:     15,
+				Description:  "Test Description",
+				UserID:       userID,
+				TimeToNotify: 10,
+			},
+			{
+				Title:        "Test Title",
+				DateAndTime:  time.Date(2024, 11, 5, 23, 24, 23, 0, time.UTC),
+				Duration:     15,
+				Description:  "Test Description",
+				UserID:       userID,
+				TimeToNotify: 10,
+			},
+			{
+				Title:        "Test Title",
+				DateAndTime:  time.Date(2024, 11, 5, 23, 24, 23, 0, time.UTC),
+				Duration:     15,
+				Description:  "Test Description",
+				UserID:       userID,
+				TimeToNotify: 10,
+			},
+			{
+				Title:        "Test Title",
+				DateAndTime:  time.Date(2024, 11, 4, 23, 24, 23, 0, time.UTC),
+				Duration:     15,
+				Description:  "Test Description",
+				UserID:       userID,
+				TimeToNotify: 10,
+			},
+		}
+
+		for _, event := range events {
+			id, err := DBstorage.Create(ctx, event, userID)
+			require.NotNil(t, id)
+			require.NoError(t, err)
+		}
+		date := time.Date(2024, 11, 5, 23, 24, 23, 0, time.UTC)
+		result, err := DBstorage.DailyList(ctx, date, userID)
+		require.NoError(t, err)
+		require.Len(t, result, 2)
+	})
+	t.Run("weekly list", func(t *testing.T) {
+		userID := uuid.New()
+
+		events := []storage.Event{
+			{
+				Title:        "Test Title",
+				DateAndTime:  time.Date(2024, 11, 6, 23, 25, 23, 0, time.UTC),
+				Duration:     15,
+				Description:  "Test Description",
+				UserID:       userID,
+				TimeToNotify: 10,
+			},
+			{
+				Title:        "Test Title",
+				DateAndTime:  time.Date(2024, 11, 12, 23, 24, 23, 0, time.UTC),
+				Duration:     15,
+				Description:  "Test Description",
+				UserID:       userID,
+				TimeToNotify: 10,
+			},
+			{
+				Title:        "Test Title",
+				DateAndTime:  time.Date(2024, 11, 14, 23, 24, 23, 0, time.UTC),
+				Duration:     15,
+				Description:  "Test Description",
+				UserID:       userID,
+				TimeToNotify: 10,
+			},
+			{
+				Title:        "Test Title",
+				DateAndTime:  time.Date(2024, 11, 15, 23, 24, 23, 0, time.UTC),
+				Duration:     15,
+				Description:  "Test Description",
+				UserID:       userID,
+				TimeToNotify: 10,
+			},
+		}
+
+		for _, event := range events {
+			id, err := DBstorage.Create(ctx, event, userID)
+			require.NotNil(t, id)
+			require.NoError(t, err)
+		}
+		date := time.Date(2024, 11, 7, 23, 24, 23, 0, time.UTC)
+		result, err := DBstorage.WeeklyList(ctx, date, userID)
+		require.NoError(t, err)
+		require.Len(t, result, 2)
+	})
 }
